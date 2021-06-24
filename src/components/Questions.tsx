626770af31c2b968b14c0b9e140263bd25aa1779
adsonatural <adsonhenriquesilva@gmail.com>
2021-06-24 17:51:27 +0200

    create a question component

---
+++ src/components/Questions.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from 'react';
+import '../styles/questions.scss';
+
+type QuestionProps = {
+  content: string;
+  author: {
+    name: string;
+    avatar: string;
+  };
+  children?: ReactNode;
+};
+
+export default function Questions(props: QuestionProps) {
+  return (
+    <div className="question">
+      <p>{props.content}</p>
+      <footer>
+        <div className="user-info">
+          <img src={props.author.avatar} alt={props.author.name} />
+          <span>{props.author.name}</span>
+        </div>
+        <div>{props.children}</div>
+      </footer>
+    </div>
+  );
+}
